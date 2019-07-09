e028525890ccefa97bf98d7dc72190a2b1ed1d1b
Valentino Wong <valentino.wong@gmail.com>
2019-07-09 19:29:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  attributes = pigeon_data.keys
+  attributes.each do |a|
+    options = pigeon_data[a].keys
+    options.each do |b|
+      names = pigeon_data[a][b]
+      names.each do |c|
+        if pigeon_list[c].nil?
+          pigeon_list[c] = {
+            a => ["#{b}"],
+          }
+        elsif pigeon_list[c][a].nil?
+          pigeon_list[c][a] = ["#{b}"]
+        else
+          pigeon_list[c][a].push("#{b}")
+        end
+      end     
+    end
+  end
+  pigeon_list
 end
