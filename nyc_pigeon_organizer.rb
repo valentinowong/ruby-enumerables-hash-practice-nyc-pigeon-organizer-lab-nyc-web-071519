ffc9ddfbaf94e146d78e20ee6dbc633d3fdf725e
Valentino Wong <valentino.wong@gmail.com>
2019-07-09 18:45:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,18 @@
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
   attributes = pigeon_data.keys
-  pigeon_data.keys.
-  pigeon_data[attributes[0]].values.each do |n|
-    n.each do |i|
-      if pigeon_list[i].nil?
-        pigeon_list[i] = {
-          :color => {},
-          :gender => {},
-          :lives => {}
-        }
+  pigeon_data.keys.each do |x|
+    pigeon_data[x].values.each do |n|
+      n.each do |i|
+        if pigeon_list[i].nil?
+          pigeon_list[i] = {
+            :color => {},
+            :gender => {},
+            :lives => {}
+          }
+        end
       end
-    end
+    end  
   end
   pigeon_list
 end
